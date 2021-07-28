d9004621e0239df3336b848e8ceb39feb5a5bee3
vitasnet <vitasnet@gmail.com>
2021-07-28 22:00:05 +0300

    dz1

---
+++ app/src/main/java/ru/geekbrains/myapplicationg/MainActivity.kt
@@ -0,0 +1,102 @@
+package ru.geekbrains.myapplicationg
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import ru.geekbrains.myapplicationg.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+    class Weather(var town: String, var temperature: Int)
+    class Repository {
+
+        private val weatherList: List<Weather>
+
+        init {
+            weatherList = listOf( Weather("Москва", 15))
+        }
+    }
+    class WeatherViewHolder(itemViev: View) : RecyclerView.ViewHolder(itemViev)
+    open class BaseViewModel : ViewModel() {
+    }
+    fun getWeather(): List<Weather> {
+        return weatherList
+    }
+    fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+        holder.bind(weatherList[position])
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(applicationContext,"Краснодар  40", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+    data class Note(val title: String, val note: String, val color: Int)
+    val newCopy = weather.copy(town = "Санкт-Петербург")
+
+
+
+
+}
